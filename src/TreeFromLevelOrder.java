88c3aa2a10df626b7bc405b5afdbc36a1db1798a
Anuj Mehta <anuj.l33t@gmail.com>
2015-04-29 10:34:54 -0400

    Added a program to make a binary tree from its level order traversal

---
+++ src/TreeFromLevelOrder.java
@@ -0,0 +1,72 @@
+/*
+ * Program to make a binary tree given the level order traversal
+ * The String "#" denotes a null Node in the Tree
+ * */
+
+import java.util.*;
+
+class TreeNode {
+	int v;
+	TreeNode left, right;
+
+	TreeNode() {
+		this.left = null;
+		this.right = null;
+	}
+
+	TreeNode(int v) {
+		this.left = null;
+		this.right = null;
+		this.v = v;
+	}
+}
+
+public class TreeFromLevelOrder {
+
+	public static void inOrder(TreeNode root) {
+		if (root == null)
+			System.out.println("Tree is Empty");
+		else
+			order(root);
+	}
+
+	public static void order(TreeNode root) {
+		if (root == null)
+			return;
+		else {
+			order(root.left);
+			System.out.print(root.v + " ");
+			order(root.right);
+		}
+	}
+
+	public static TreeNode formTree(String[] s) {
+		if (s.length == 0 || s[0].equals("#"))
+			return null;
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		TreeNode root = new TreeNode(Integer.parseInt(s[0]));
+		q.add(root);
+		int l = s.length - 1;
+		int i = 0;
+		while (!q.isEmpty()) {
+			TreeNode t = q.poll();
+			if (2 * i + 1 <= l && !s[2 * i + 1].equals("#")) {
+				t.left = new TreeNode(Integer.parseInt(s[2 * i + 1]));
+				q.add(t.left);
+			}
+			if (2 * i + 2 <= l && !s[2 * i + 2].equals("#")) {
+				t.right = new TreeNode(Integer.parseInt(s[2 * i + 2]));
+				q.add(t.right);
+			}
+			i++;
+		}
+		return root;
+	}
+
+	public static void main(String args[]) {
+		String s = "1 2 3 4 # 6 7";
+		String st[] = s.split(" ");
+		TreeNode root = formTree(st);
+		inOrder(root);
+	}
+}
