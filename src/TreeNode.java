6584b2ddd8a37199120ec705a61a62c12a06b15d
Anuj Mehta <anuj.l33t@gmail.com>
2015-05-07 11:48:31 -0400

    Added a file to iterate through binary trees without using recursion

---
+++ src/TreeNode.java
@@ -0,0 +1,21 @@
+package src;
+
+public class TreeNode {
+	int v;
+	TreeNode left,right;
+	TreeNode()
+	{
+		this.left=null;
+		this.right=null;
+	}
+	TreeNode(int v)
+	{
+		this.v=v;
+		this.left=null;
+		this.right=null;
+	}
+	public String toString()
+	{
+		return this.v+"";
+	}
+}
